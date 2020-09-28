2b4f2b22d2818758a333442edbeb8c060429d234
ASUS <se.zanganeh@yahoo.com>
2020-09-28 20:31:25 +0330

    video 10-start store app

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(Store());
+
+class Store extends StatefulWidget {
+  @override
+  _StoreState createState() => _StoreState();
+}
+
+class _StoreState extends State<Store> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text(" Video 9 !!!! "),
-        ),
-        body: Center(
-          child: Text(" Hello!!!! "),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.weekend),
+          title: Text(
+            "Store",
+            style: TextStyle(color: Colors.black45),
+          ),
+          centerTitle: true,
+          leading: Icon(
+            Icons.arrow_back,
+            color: Colors.black45,
+          ),
+          backgroundColor: Colors.white,
+          elevation: 0,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.map,
+                color: Colors.black45,
+              ),
+            )
+          ],
         ),
       ),
-    ));
+    );
+  }
+}
---
+++ lib/video 9.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(" Video 9 !!!! "),
+        ),
+        body: Center(
+          child: Text(" Hello!!!! "),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.weekend),
+        ),
+      ),
+    ));
