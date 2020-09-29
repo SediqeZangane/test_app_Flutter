a92b31d27fdf2153b2a93449691c2284a2ac7345
ASUS <se.zanganeh@yahoo.com>
2020-09-29 23:33:38 +0330

    viedo13-body

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,17 @@ class _StoreState extends State<Store> {
             )
           ],
         ),
+        body: Padding(
+          padding: EdgeInsets.all(15),
+          child: GridView.count(
+            crossAxisCount: 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 15,
+            children: List.generate(10, (int position) {
+              return generateItem();
+            }),
+          ),
+        ),
         bottomNavigationBar: ShopBottomNavigator(),
         floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.red[900],
@@ -45,4 +56,35 @@ class _StoreState extends State<Store> {
       ),
     );
   }
+}
+
+Card generateItem() {
+  return Card(
+    shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(30))),
+    elevation: 4,
+    child: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            width: 90,
+            height: 90,
+            child: Image.network(
+                "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"),
+          ),
+          Text(
+            "قیمت",
+            style: TextStyle(
+                fontFamily: "Vazir", color: Colors.red[700], fontSize: 14),
+          ),
+          Text(
+            "عنوان",
+            style: TextStyle(
+                fontFamily: "Vazir", color: Colors.blueGrey, fontSize: 14),
+          )
+        ],
+      ),
+    ),
+  );
 }
