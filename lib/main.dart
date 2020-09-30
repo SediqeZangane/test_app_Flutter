5afe6d2c6712269ea0e152aa906407b5b8af35f4
ASUS <se.zanganeh@yahoo.com>
2020-09-30 10:56:18 +0330

    viedo15-json.decode.pulseJson

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:test_app/viedo12-ShopBottomNavigator.dart';
 import 'package:http/http.dart';
+import 'Product-video15.dart';
 
 void main() => runApp(Store());
 
@@ -12,6 +13,8 @@ class Store extends StatefulWidget {
 }
 
 class _StoreState extends State<Store> {
+  List<Product> _items = [];
+
   @override
   void initState() {
     // TODO: implement initState
@@ -51,8 +54,8 @@ class _StoreState extends State<Store> {
             crossAxisCount: 2,
             crossAxisSpacing: 10,
             mainAxisSpacing: 15,
-            children: List.generate(10, (int position) {
-              return generateItem();
+            children: List.generate(_items.length, (int position) {
+              return generateItem(_items[position]);
             }),
           ),
         ),
@@ -70,10 +73,18 @@ class _StoreState extends State<Store> {
     var url = "http://welearnacademy.ir/flutter/products_list.json";
     Response response = await get(url);
     print(utf8.decode(response.bodyBytes));
+    setState(() {
+      var productJson = json.decode(utf8.decode(response.bodyBytes));
+      for (var i in productJson) {
+        var productItem = Product(i['product_name'], i['id'], i['price'],
+            i['image_url'], i['off'], i['description']);
+        _items.add(productItem);
+      }
+    });
   }
 }
 
-Card generateItem() {
+Card generateItem(Product product) {
   return Card(
     shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.all(Radius.circular(30))),
@@ -83,18 +94,17 @@ Card generateItem() {
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Container(
-            width: 90,
-            height: 90,
-            child: Image.network(
-                "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"),
+            width: 130,
+            height: 130,
+            child: Image.network(product.imageUrl),
           ),
           Text(
-            "قیمت",
+            product.price,
             style: TextStyle(
-                fontFamily: "Vazir", color: Colors.red[700], fontSize: 14),
+                fontFamily: "Vazir", color: Colors.red[700], fontSize: 16),
           ),
           Text(
-            "عنوان",
+            product.productName,
             style: TextStyle(
                 fontFamily: "Vazir", color: Colors.blueGrey, fontSize: 14),
           )
